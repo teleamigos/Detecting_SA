ca2fb8f5746b9dc1e0efdee4c396f9fc8839f41f
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-02-16 14:33:34 -0600

    Basic class node

--- Detecting_SA/Node.cpp
+++ Detecting_SA/Node.cpp
@@ -5,10 +5,12 @@
 //  Created by Jorge Orlando Gonzalez  on 2/16/20.
 //  Copyright © 2020 Jorge Orlando Gonzalez . All rights reserved.
 //
-
+/*-----------------------------------------------------------------------------------------------------
+                                     class Node implementation
+-----------------------------------------------------------------------------------------------------*/
 #include "Node.hpp"
 #include <vector>
-
+/*Constructors*/
 Node::Node(int id,std::vector<int> _neighboor):ID(id),neighboor(_neighboor)
 {
     //Exceptions
@@ -18,7 +20,7 @@ Node::Node(const Node &nodo){
     this->ID=nodo.ID;
     this->neighboor=nodo.neighboor;
 }
-
+/*Get*/
 int Node::GetID()const
 {
     return this->ID;
@@ -28,7 +30,7 @@ std::vector<int> Node::GetNeighboors()const
 {
     return this->neighboor;
 }
-
+/*Set*/
 void Node::SetID(int id)
 {
     this->ID=id;
--- Detecting_SA/Node.hpp
+++ Detecting_SA/Node.hpp
@@ -8,24 +8,32 @@
 
 #ifndef Node_hpp
 #define Node_hpp
-
+/*-----------------------------------------------------------------------------------------------------
+                                     class Node definition
+-----------------------------------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <string>
 
 class Node{
 private:
     int ID=0;
     std::vector<int> neighboor{1};
+    //std::string MAC_add;
 public:
+    /*Constructors*/
     Node()=default;
     Node(int id,std::vector<int> _neighboor);
     Node(const Node &nodo);
+    /*Get*/
     int GetID()const;
     std::vector<int> GetNeighboors()const;
+    /*Set*/
     void SetID(int id);
     void SetNeighboors(std::vector<int> v);
-    /*----------*/
+    /*Methods*/
+    /* void Sender(uint package);*/
 };
 
 #endif /* Node_hpp */
