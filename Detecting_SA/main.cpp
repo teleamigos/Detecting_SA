d56b3a29761982a1be7ddddb001a956ecf05e589
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-02-16 13:19:43 -0600

    Initial Commit

---
+++ Detecting_SA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Detecting_SA
+//
+//  Created by Jorge Orlando Gonzalez  on 2/16/20.
+//  Copyright © 2020 Jorge Orlando Gonzalez . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
