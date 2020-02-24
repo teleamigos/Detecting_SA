a3cf7dbc77b2f5967dd637090fa57c5c5eb996c4
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-02-23 22:29:48 -0600

    Codigo completo y comentado

--- Detecting_SA/Node.hpp
+++ Detecting_SA/Node.hpp
@@ -15,25 +15,33 @@
 using namespace std;
 class Node{
 private:
-    unsigned char ID;
-    unsigned char type_m='0';
-    string pack;
+    unsigned char ID; //1 byte para el 1d
+    unsigned char type_m='0'; //Mesnaje hello (1 byte)
+    string pack; // guarda en 2 bytes
 public:
-    /*Constructors*/
+    /*Constructors
+     Construyen la clase de 3 diferentes formas:
+     1.- por defecto (como se inicilizan los miembros de la clase)
+     2.- Pasando dos argumentos ID y el tipo de mensaje
+     3.- Pasando otro objecto de la clase Node*/
     Node()=default;
     Node(unsigned char id,unsigned char type);
     Node(const Node &nodo);
-    /*Destructor*/
+    /*Destructor
+     Destruye la clase*/
     ~Node()=default;
-    /*Get*/
+    /*Get
+     Nos permite obtener datos los miembros de la clase*/
     unsigned char GetID()const;
     unsigned char GetTypeM()const;
     string GetPack()const;
-    /*Set*/
+    /*Set
+     Nos permite modificar el contenido de los datos miembro*/
     void SetID(unsigned char id);
     void SetTypeM(unsigned char type);
     void SetPack(string p);
-    /*Methods*/
+    /*Methods
+     Empaqueta ID y tipo en una variable*/
     void Pack();// Pack information...
 };
 
